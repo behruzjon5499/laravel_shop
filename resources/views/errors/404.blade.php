809348b1efd294f3126811b3a6b83d1ed3fd3148
bexruz <behruztoxirov909515499@gmail.com>
2023-12-12 17:52:19 +0500

    error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('The page you are looking for might have been removed had its name changed or is temporarily unavailable.'))
