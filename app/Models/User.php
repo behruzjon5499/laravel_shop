d7e136728fc6e2b483087f7e44dd330784fb283b
bexruz <behruztoxirov909515499@gmail.com>
2023-12-14 14:31:47 +0500

    admin panel access for only my email

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, 'behruztoxirov909515499@gmail.com');
     }
 
     protected function name(): Attribute
